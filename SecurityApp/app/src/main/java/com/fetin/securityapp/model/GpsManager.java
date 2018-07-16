1cbb3f0384e0849f2c3bb9ccd35f00c2417bdacf
EnsleyEC <ensleyfmr@hotmail.com>
2018-07-16 16:08:28 -0300

    Implementando o GPS

---
+++ SecurityApp/app/src/main/java/com/fetin/securityapp/model/GpsManager.java
@@ -0,0 +1,29 @@
+package com.fetin.securityapp.model;
+
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v7.app.AppCompatActivity;
+
+public class GpsManager extends AppCompatActivity{
+
+    private transient LocationManager locationManager;
+    private transient LocationListener locationListener;
+    /*
+    public void getLocalization() {
+        Looper.prepare();
+        locationManager = (LocationManager) this.getContext().getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new MyLocationListener();
+
+        long minTime = 0;
+        float minDistance = 0;
+        String provider;
+        if (Geolocation.LEVEL_EXACT.equals(level)) {
+            provider = LocationManager.GPS_PROVIDER;
+        } else {
+            provider = LocationManager.NETWORK_PROVIDER;
+        }
+        locationManager.requestLocationUpdates(provider, minTime, minDistance, locationListener);
+        Looper.loop();
+        */
+
+}
