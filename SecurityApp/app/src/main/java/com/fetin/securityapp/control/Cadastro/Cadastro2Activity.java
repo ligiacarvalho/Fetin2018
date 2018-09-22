a244c76fd561e56f1ca9e3112be94de3990d1784
Ensley <ensleyfmr@hotmail.com>
2018-09-22 15:10:41 -0300

    merge feito!

--- SecurityApp/app/src/main/java/com/fetin/securityapp/control/Cadastro/Cadastro2Activity.java
+++ SecurityApp/app/src/main/java/com/fetin/securityapp/control/Cadastro/Cadastro2Activity.java
@@ -157,91 +157,83 @@ public class Cadastro2Activity extends AppCompatActivity {
         Intent intent = new Intent(this, LoginActivity.class);
         boolean resp = verificaEntradaDeDados();
 
-<<<<<<< HEAD
         if (resp) {
-=======
-        if(resp){
-            getDadosDosCampos1();
->>>>>>> c54d8958304f48c2a85766611996ebcbefc1d696
-            getDadosDosCampos2();
+            if (resp) {
+                getDadosDosCampos2();
+                finish();
+                startActivity(intent);
+            }
+
+        }
+    }
+        //voltar para cadastro pessoal
+        public void voltarCadastro (View view){
+            Intent intent = new Intent(this, Cadastro1Activity.class);
             finish();
             startActivity(intent);
         }
 
+        public void getDadosDosCampos2 () {
+            // Referenciando os componentes da interface
+            EditText campoModeloCelular = findViewById(R.id.CampoModeloCelular);
+            RadioButton campoNumeroChip1 = findViewById(R.id.radioButtonNumChip1);
+            RadioButton campoNumeroChip2 = findViewById(R.id.radioButtonNumChip2);
+            EditText numeroDoChip1 = findViewById(R.id.EditTextNumChip1);
+            EditText numeroDoChip2 = findViewById(R.id.EditTextNumChip2);
+            EditText campoIMEI1 = findViewById(R.id.campoIMEI1);
+            EditText campoIMEI2 = findViewById(R.id.campoIMEI2);
+
+            //instanciando o celular
+            Celular novoCelular = new Celular();
+
+            novoCelular.setModelo(campoModeloCelular.getText().toString());
+            novoCelular.setChip1(campoNumeroChip1.getText().toString());
+            novoCelular.setChip2(campoNumeroChip2.getText().toString());
+            novoCelular.setImei1(campoIMEI1.getText().toString());
+            novoCelular.setImei2(campoIMEI2.getText().toString());
+
+            CelularDAO daoC = new CelularDAO();
+
+            CelularDAO.Cel_cadastrado = novoCelular;
+
+            UsuarioDAO.user_cadastrado.setCelularP(novoCelular);
+
+            UsuarioDAO.dao.inserir(UsuarioDAO.user_cadastrado);
+
+        }
+
+        //criando mascaras
+        public void MascaraChip2 () {
+            // Referenciando o número o campo do número do CHIP 1, para pode manipular os dados de lá
+            EditText numeroDoChipDois = findViewById(R.id.EditTextNumChip2);
+
+            // Passando qual é o padrão de telefone que eu vou querer que o usuário digite
+            SimpleMaskFormatter mascaraSimplesDeTelefone = new SimpleMaskFormatter("(NN) NNNNN-NNNN");
+
+            // Passando para o objeto que vai gerenciar o formato do telefone, qual é o editText e qual é o formato que queremos
+            MaskTextWatcher objetoQueVaiGerenciarOFormatoDoTelefone = new MaskTextWatcher(numeroDoChipDois, mascaraSimplesDeTelefone);
+
+            // Qualquer alteração de texto no EditText do número do Chip1, o método abaixo vai usar botar
+            // o "objetoQueVaiGerenciarOFormatoDoTelefone" para trabalhar.
+            numeroDoChipDois.addTextChangedListener(objetoQueVaiGerenciarOFormatoDoTelefone);
+
+
+        }
+
+        public void MascaraChip1 () {
+            // Referenciando o número o campo do número do CHIP 1, para pode manipular os dados de lá
+            EditText numeroDoChipUm = findViewById(R.id.EditTextNumChip1);
+
+            // Passando qual é o padrão de telefone que eu vou querer que o usuário digite
+            SimpleMaskFormatter mascaraSimplesDeTelefone = new SimpleMaskFormatter("(NN) NNNNN-NNNN");
+
+            // Passando para o objeto que vai gerenciar o formato do telefone, qual é o editText e qual é o formato que queremos
+            MaskTextWatcher objetoQueVaiGerenciarOFormatoDoTelefone = new MaskTextWatcher(numeroDoChipUm, mascaraSimplesDeTelefone);
+
+            // Qualquer alteração de texto no EditText do número do Chip1, o método abaixo vai usar botar
+            // o "objetoQueVaiGerenciarOFormatoDoTelefone" para trabalhar.
+            numeroDoChipUm.addTextChangedListener(objetoQueVaiGerenciarOFormatoDoTelefone);
+
+
+        }
     }
-
-    //voltar para cadastro pessoal
-    public void voltarCadastro(View view) {
-        Intent intent = new Intent(this, Cadastro1Activity.class);
-        finish();
-        startActivity(intent);
-    }
-
-    public void getDadosDosCampos2() {
-        // Referenciando os componentes da interface
-        EditText campoModeloCelular = findViewById(R.id.CampoModeloCelular);
-        RadioButton campoNumeroChip1 = findViewById(R.id.radioButtonNumChip1);
-        RadioButton campoNumeroChip2 = findViewById(R.id.radioButtonNumChip2);
-        EditText numeroDoChip1 = findViewById(R.id.EditTextNumChip1);
-        EditText numeroDoChip2 = findViewById(R.id.EditTextNumChip2);
-        EditText campoIMEI1 = findViewById(R.id.campoIMEI1);
-        EditText campoIMEI2 = findViewById(R.id.campoIMEI2);
-
-        //instanciando o celular
-        Celular novoCelular = new Celular();
-
-        novoCelular.setModelo(campoModeloCelular.getText().toString());
-        novoCelular.setChip1(campoNumeroChip1.getText().toString());
-        novoCelular.setChip2(campoNumeroChip2.getText().toString());
-        novoCelular.setImei1(campoIMEI1.getText().toString());
-        novoCelular.setImei2(campoIMEI2.getText().toString());
-
-        CelularDAO daoC = new CelularDAO();
-
-        CelularDAO.Cel_cadastrado = novoCelular;
-
-        UsuarioDAO.user_cadastrado.setCelularP(novoCelular);
-
-        UsuarioDAO.dao.inserir(UsuarioDAO.user_cadastrado);
-
-    }
-
-    public void getDadosDosCampos1(){
-
-
-    }
-    //criando mascaras
-    public void MascaraChip2() {
-        // Referenciando o número o campo do número do CHIP 1, para pode manipular os dados de lá
-        EditText numeroDoChipDois = findViewById(R.id.EditTextNumChip2);
-
-        // Passando qual é o padrão de telefone que eu vou querer que o usuário digite
-        SimpleMaskFormatter mascaraSimplesDeTelefone = new SimpleMaskFormatter("(NN) NNNNN-NNNN");
-
-        // Passando para o objeto que vai gerenciar o formato do telefone, qual é o editText e qual é o formato que queremos
-        MaskTextWatcher objetoQueVaiGerenciarOFormatoDoTelefone = new MaskTextWatcher(numeroDoChipDois, mascaraSimplesDeTelefone);
-
-        // Qualquer alteração de texto no EditText do número do Chip1, o método abaixo vai usar botar
-        // o "objetoQueVaiGerenciarOFormatoDoTelefone" para trabalhar.
-        numeroDoChipDois.addTextChangedListener(objetoQueVaiGerenciarOFormatoDoTelefone);
-
-
-    }
-
-    public void MascaraChip1() {
-        // Referenciando o número o campo do número do CHIP 1, para pode manipular os dados de lá
-        EditText numeroDoChipUm = findViewById(R.id.EditTextNumChip1);
-
-        // Passando qual é o padrão de telefone que eu vou querer que o usuário digite
-        SimpleMaskFormatter mascaraSimplesDeTelefone = new SimpleMaskFormatter("(NN) NNNNN-NNNN");
-
-        // Passando para o objeto que vai gerenciar o formato do telefone, qual é o editText e qual é o formato que queremos
-        MaskTextWatcher objetoQueVaiGerenciarOFormatoDoTelefone = new MaskTextWatcher(numeroDoChipUm, mascaraSimplesDeTelefone);
-
-        // Qualquer alteração de texto no EditText do número do Chip1, o método abaixo vai usar botar
-        // o "objetoQueVaiGerenciarOFormatoDoTelefone" para trabalhar.
-        numeroDoChipUm.addTextChangedListener(objetoQueVaiGerenciarOFormatoDoTelefone);
-
-
-    }
-}
